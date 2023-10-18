a59864dcbc411bc70d13c6bd96a64e0ae496519f
PixMate <94467160+pixmaate@users.noreply.github.com>
2023-10-18 09:58:08 +0200

    basic functions created

---
+++ jsconfig.js
@@ -0,0 +1,17 @@
+
+
+function add(inputOne,inputTwo) {
+    return inputOne + inputTwo
+}
+
+function subtract(inputOne,inputTwo) {
+    return inputOne - inputTwo
+}
+
+function multiply(inputOne,inputTwo) {
+    return inputOne * inputTwo
+}
+
+function divide(inputOne,inputTwo) {
+    return inputOne / inputTwo
+}
